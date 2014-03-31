ee4076dce3c61f161fd6b394c2cd3124c58c3c13
cosmeo <mbintern2@Matchbox-6.local>
2014-03-31 17:45:48 -0500

    11th Commit

--- js/functions.js
+++ js/functions.js
@@ -13,6 +13,9 @@
 
 	/* trigger when page is ready */
 	$(document).ready(function (){
+
+
+
 	
 		// Smooth Scrolling
 		$('.nav-collapse a[href*=#]:not([href=#]), #go-to-top a[href*=#]:not([href=#]), .scroll_down a[href*=#]:not([href=#])').click(function() {
@@ -32,6 +35,46 @@
 	    
 
 		}); // Ending Smooth Scrolling function
+
+		// Hide Header on on scroll down
+		var didScroll;
+		var lastScrollTop = 0;
+		var delta = 5;
+		var navbarHeight = $('nav').outerHeight();
+
+		$(window).scroll(function(event){
+		    didScroll = true;
+		});
+
+		setInterval(function() {
+		    if (didScroll) {
+		        hasScrolled();
+		        didScroll = false;
+		    }
+		}, 250);
+
+		function hasScrolled() {
+		    var st = $(this).scrollTop();
+		    
+		    // Make sure they scroll more than delta
+		    if(Math.abs(lastScrollTop - st) <= delta)
+		        return;
+		    
+		    // If they scrolled down and are past the navbar, add class .nav-up.
+		    // This is necessary so you never see what is "behind" the navbar.
+		    if (st > lastScrollTop && st > navbarHeight){
+		        // Scroll Down
+		        $('nav').removeClass('nav-down').addClass('nav-up');
+		    } else {
+		        // Scroll Up
+		        if(st + $(window).height() < $(document).height()) {
+		            $('nav').removeClass('nav-up').addClass('nav-down');
+		        }
+		    }
+		    
+		    lastScrollTop = st;
+		}
+
 
 
 		// Stop Youtube videos when switching frames
