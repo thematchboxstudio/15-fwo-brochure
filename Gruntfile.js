4ea6550a6e0582fd3f5f8ecbc8b2e36120288dbb
Sean Walker <sean@matchboxstudio.com>
2014-04-15 12:03:40 -0500

    Optimized
    Optimized the images, and put them in the ‘i’ folder.
    
    ‘img’ folder is untouched.
    
    Also added Grunt, and a build process. So once you get grunt set up,
    ‘grunt build’ will build out an optimized file structure in a ‘build’
    folder.
    
    I can explain all this next time you’re here.

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,15 +8,10 @@ module.exports = function(grunt) {
 		uglify: { /// UGLIFY - minimizes and combines js files. all {src} files into {dest}.
 			build: {
 				files: {
-	        'library/js/build/global.min.js': ['library/js/libs/*', 'library/js/scripts.js'],
-	        'library/js/build/typekit.wp-editor.js': ['library/js/typekit.wp-editor.js']
-	      }
+	        		'build/js/functions.js': ['js/functions.js', 'js/responsive-nav.js'],
+	      		}
 			}
 		},
-		/* UNCOMMENT THIS (and the below) TO CHECK YOUR JS, I COMMENTED IT OUT BY DEFAULT CAUSE THERE ARE ERRORS THAT WE ARE IGNORING :P
-		jshint: {
-			all: ['Gruntfile.js',  'library/js/scripts.js']
-		},*/
  		svgmin: { /// SVG MIN - MINIMIZES SVG FILES IN THE {images} FOLDER AND PUTS THEM IN THE {img} FOLDER
 			options: {                  // Configuration that will be passed directly to SVGO
 				plugins: [
@@ -28,21 +23,11 @@ module.exports = function(grunt) {
 			dist: {                     // Target
 				files: [{               // Dictionary of files
 					expand: true,       // Enable dynamic expansion.
-					cwd: 'library/images',     // Src matches are relative to this path.
+					cwd: 'build/i',     // Src matches are relative to this path.
 					src: ['**/*.svg'],  // Actual pattern(s) to match.
-					dest: 'library/img/',       // Destination path prefix.
-					ext: '.min.svg'     // Dest filepaths will have this extension.
+					dest: 'build/i/',       // Destination path prefix.
+					ext: '.svg'     // Dest filepaths will have this extension.
 					// ie: optimise img/src/branding/logo.svg and store it in img/branding/logo.min.svg
-				}]
-			}
-		},
-		imagemin: {  /// IMAGEMIN - optimizes all source images {cwd} and spits them into {dest}.
-			dynamic: {
-				files: [{
-					expand: true,
-					cwd: 'library/images/',
-					src: ['**/*.{png,jpg,gif}'],
-					dest: 'library/img/'
 				}]
 			}
 		},
@@ -53,15 +38,13 @@ module.exports = function(grunt) {
 		      imageAlpha: true,
 		      quitAfter: true
 		    },
-		    src: ['library/img/', 'library/img/*/']
+		    src: ['build/i/', 'build/i/*/']
 		  }
 		},
 		autoprefixer: { /// AUTOPREFIXER
 				dist: {
 					files: {
-						'library/css/build/style.css': 'library/css/build/style.css',
-						'library/css/build/login.css': 'library/css/build/login.css',
-						'library/css/build/ie.css': 'library/css/build/ie.css'
+						'build/css/style.css': 'build/css/style.css',
 						},
 						options: {
 							browsers: ['last 3 versions']
@@ -71,55 +54,47 @@ module.exports = function(grunt) {
 		cssmin: {
 			minify: {
 				expand: true,
-				cwd: 'library/css/build/',
+				cwd: 'build/css/',
 				src: ['*.css'],
-				dest: 'library/css/build/',
+				dest: 'build/css/',
 			},
 			options: {
 				keepSpecialComments: 0,
 			}
 		},
-		uncss: {
-			dist: {
-				files: {
-				'library/css/build/style.css': ['*.php']
-				},
-				options: {
-					ignore: [':hover', ':focus', ':before', ':after','.js-nav-active body:not(.home) .header', 'body:not(.home) .header', "#colorbox", '#cboxPrevious','#cboxNext', '#cboxContent', '#cboxLoadedContent', '#cboxWrapper', '.cboxPhoto' ],
-					stylesheets: ["library/css/build/style.css"],
-					urls: ["http://beta.matchboxstudio.com/vital/","http://beta.matchboxstudio.com/vital/classes/","http://beta.matchboxstudio.com/vital/about-us/","http://beta.matchboxstudio.com/vital/pricing-and-memberships/","http://beta.matchboxstudio.com/vital/our-instructors/","http://beta.matchboxstudio.com/vital/studio-gallery/"],
-					report: 'min',
-
-				}
-			},
-		},
 		copy: { /// THIS IS THE PART OF THE BUILD PROCESS THAT COPIES THE NEEDED FILES TO THE NEW BUILD FOLDER THEME. YOU JUST UPLOAD THAT THEME SINCE IT HAS ONLY THE FILES THAT ARE NEEDED
 		  main: {
 		    files: [
-		      {expand: true, src: ['*.php', '*.ico', '*.png', '*.css'], dest: '../mb-build/', filter: 'isFile'},
- 		      {expand: true, flatten: true, src: ['library/*.php'], dest: '../mb-build/library/', filter: 'isFile'},
- 		      {expand: true, flatten: true, src: ['library/img/**'], dest: '../mb-build/library/img/', filter: 'isFile'},
- 		      {expand: true, cwd: 'library/img/', src: ['**'], dest: '../mb-build/library/img/'},
+ 		      {expand: true, flatten: true, src: ['i/**'], dest: 'build/i/', filter: 'isFile'},
+ 		      {expand: true, flatten: true, src: ['css/style.css'], dest: 'build/css/', filter: 'isFile'},
+ 		      {expand: true, flatten: true, src: ['js/jquery-1.10.2.min.js'], dest: 'build/js/', filter: 'isFile'},
+ 		      {expand: true, cwd: 'i/', src: ['**'], dest: 'build/i/'},
 
 		    ]
 		  }
-		}
-	}); // END CONFIG
+		},
+	  htmlmin: {                                     // Task
+	    dist: {                                      // Target
+	      options: {                                 // Target options
+	        removeComments: true,
+	        collapseWhitespace: true
+	      },
+	      files: {                                   // Dictionary of files
+	        'build/index.html': 'index.html',     // 'destination': 'source'
+	      }
+	    },
+	  }
+  	}); // END CONFIG
 
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-svgmin');
-	grunt.loadNpmTasks('grunt-contrib-imagemin');
 	grunt.loadNpmTasks('grunt-imageoptim');
-
 	grunt.loadNpmTasks('grunt-autoprefixer');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-htmlmin');
+	grunt.loadNpmTasks('grunt-contrib-copy');
 
-	grunt.loadNpmTasks('grunt-uncss');
+	grunt.registerTask('build', ['uglify', 'copy', 'autoprefixer', 'htmlmin', 'svgmin', /*'imageoptim',*/ 'cssmin',   ]);
 
-	// Default task(s).
-	grunt.registerTask('default', ['uglify', 'svgmin', 'imagemin', 'imageoptim', 'autoprefixer', 'cssmin' ]);
-	grunt.registerTask('images', ['svgmin','imagemin', 'imageoptim']);
-	grunt.registerTask('text', ['uglify','autoprefixer']);
-	grunt.registerTask('build', ['uglify','autoprefixer', 'uncss', 'cssmin', /*'svg-sprites','svgmin', 'imagemin', 'imageoptim'*/]);
 
 };
